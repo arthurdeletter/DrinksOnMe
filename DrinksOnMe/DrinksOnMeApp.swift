ac914a4e86c23c0ed59a9975497c27d18497f6b5
Arthur De Letter <arthur.deletter@hotmail.com>
2022-10-11 19:01:20 +0200

    Initial Commit

---
+++ DrinksOnMe/DrinksOnMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinksOnMeApp.swift
+//  DrinksOnMe
+//
+//  Created by Arthur De letter on 11/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct DrinksOnMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
